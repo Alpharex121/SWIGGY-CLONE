259367fbb9c13675601149c7c37e403591693c22
Alpharex121 <arpitkoshta08@gmail.com>
2024-04-05 22:11:57 +0530

    first commit

---
+++ src/utils/useRestaurantDetail.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const useRestaurantDetail = (resId) => {
+  const [restaurantDetail, setRestaurantDetail] = useState([]);
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
+  async function getMenu() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6126255&lng=77.04108959999999&restaurantId=" +
+        resId
+    );
+    // console.log(data);
+
+    const menuList = await data.json();
+
+    setRestaurantDetail(menuList?.data?.cards[0]?.card?.card?.info);
+  }
+  return restaurantDetail;
+};
+
+export default useRestaurantDetail;
